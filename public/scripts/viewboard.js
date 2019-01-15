b78cd6f952d9223efcedbc77ae9eb3e3b353e668
John Doe <patrick.gould94@gmail.com>
2019-01-15 16:10:07 -0500

    p5 crashing while hosted on server

--- public/scripts/viewboard.js
+++ public/scripts/viewboard.js
@@ -1,32 +1,62 @@
 // placeholder game tiles code
-let testGame;
-function start(p) {
-    p.setup = function() {
-        p.createCanvas(805, 805);
-    };
+// let testGame;
+// function start(p) {
+//     p.setup = function() {
+//         p.createCanvas(805, 805);
+//     };
 
-    p.draw = function() {
-        $.get("/api/testGen/game")
-            .then(function (game) {
-                if (game) {
-                    console.log(game);
-                    testGame = new Game("test Id", "player1 Id", "player2 Id", game);
-                    testGame.drawTiles();
+//     p.draw = function() {
+//         $.get("/api/testGen/game")
+//             .then(function (game) {
+//                 if (game) {
+//                     console.log(game);
+//                     testGame = new Game("test Id", "player1 Id", "player2 Id", game);
+//                     testGame.drawTiles();
 
                     
-                }
-            })
-            .catch(function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-    };
-}
-let myp5 = new p5(start, testGame); 
+//                 }
+//             })
+//             .catch(function (err) {
+//                 if (err) {
+//                     console.log(err);
+//                 }
+//             });
+//     };
+// }
+// let myp5 = new p5(start, testGame); 
+let newGame;
+$.get("/api/testGen/game")
+    .then(function (game) {
+        if (game) {
+            newGame = game;   
+        }
+    })
+    .catch(function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
 
+// function setup() {
+//     createCanvas(805, 805); 
+//     background(105);  
+// }
 
-        
+// function draw() {
+//     for (let i = 0; i < 10; i++) {
+//         for (let j = 0; j < 10; i++) {
+//             let width = 50;
+//             let height = 50;
+//             let x = i * width + 2;
+//             let y = j * height + 2;
+
+//             stroke(255);
+//             fill(0);
+//             rect(x, y, width, height);
+            
+//         }
+//     }
+// }
 
 
 
