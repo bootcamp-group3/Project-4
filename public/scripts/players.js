01dd7b5a1b14d1331db5db3c0fc5c2946047e67d
Ordolph <austin.t.randolph@gmail.com>
2019-01-16 16:48:34 -0500

    removed stringify

--- public/scripts/players.js
+++ public/scripts/players.js
@@ -1,6 +1,6 @@
 function onSignIn(googleUser) {
     const profile = googleUser.getBasicProfile();
-    const id = JSON.stringify(profile.getId()); // Do not send to your backend! Use an ID token instead.
+    const id = profile.getId(); // Do not send to your backend! Use an ID token instead.
     const name = profile.getName();
     const icon = profile.getImageUrl();
 
