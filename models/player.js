202924831a3db3bea7bc92e00dde0668fb7ef482
Ordolph <austin.t.randolph@gmail.com>
2019-01-16 16:33:22 -0500

    SQL WHY

--- models/player.js
+++ models/player.js
@@ -1,7 +1,7 @@
 module.exports = function (sequelize, DataTypes) {
     var Player = sequelize.define("Player", {
         googleId: {
-            type: DataTypes.INTEGER,
+            type: DataTypes.BIGINT,
             allowNull: false,
         },
         name: {
